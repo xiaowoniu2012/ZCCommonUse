8f4fa4ec65b9c4e3c4388cd6b7e83b8824ae1673
zichaochu <zichaochu@chewdeMacBook-Air.local>
2016-09-02 16:40:26 +0800

    UINetworkActivityIndicator

---
+++ UINetworkActivityIndicator/UINetworkActivityIndicator.h
@@ -0,0 +1,16 @@
+//
+//  UINetworkActivityIndicator.h
+//  CodeLibary
+//
+//  Created by zichaochu on 16/9/2.
+//  Copyright © 2016年 linxun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UINetworkActivityIndicator : NSObject
+
++ (void)showNetworkActivityIndicator;
++ (void)hideNetworkActivityIndicator;
+
+@end
